3ec5791114dffd1dafae065ff20458e998671f15
Mateen <91126296+thegeeekynerd@users.noreply.github.com>
2025-11-03 22:22:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
-load_dotenv()
-os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
 
 
 
